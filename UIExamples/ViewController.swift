d36fa3bac7095668c3a9cbe510caf4e4c564238f
Manoli <igielskv@icloud.com>
2020-07-06 18:14:32 +0200

    Adds AlertController with respective 'OK' AlertAction to dismiss it.

--- UIExamples/ViewController.swift
+++ UIExamples/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didPressButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Alert Title", message: "Alert Message", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
+            print("all done!")
+        }
+        alertController.addAction(alertAction)
+        present(alertController,animated: true, completion: nil)
+    }
+    
 }
 
